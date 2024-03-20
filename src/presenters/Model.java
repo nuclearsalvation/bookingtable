cf467e3e057e057b37bbdb593fc9adccbf2b6598
Anton Golubev <xiagenius@gmail.com>
2024-03-20 21:00:20 +0300

    model

---
+++ src/presenters/Model.java
@@ -0,0 +1,14 @@
+package presenters;
+
+import models.Table;
+
+import java.util.Collection;
+import java.util.Date;
+
+public interface Model {
+
+    Collection<Table> loadTables();
+
+    int reservationTable(Date reservationDate, int tableNo, String name);
+
+}
