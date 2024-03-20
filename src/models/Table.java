f93149899a82119f5ffa8117ef56f37e3262b9aa
Anton Golubev <xiagenius@gmail.com>
2024-03-20 20:59:19 +0300

    table

---
+++ src/models/Table.java
@@ -0,0 +1,44 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Locale;
+
+public class Table {
+
+    private static int counter;
+    private int no;
+
+    private Collection<Reservation> reservations = new ArrayList<>();
+
+    public int getNo() {
+        return no;
+    }
+
+    public Collection<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public Table(){
+        no = ++counter;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.getDefault(), "Столик #%d", no);
+    }
+    
+    public void removeReservation(int id){
+        Reservation requiredReservation = null;
+        for(Reservation reservation: reservations){
+            if (reservation.getId() == id){
+                requiredReservation = reservation;
+            }
+
+        }
+        if (requiredReservation != null){
+            reservations.remove(requiredReservation);
+        }
+    }
+
+}
